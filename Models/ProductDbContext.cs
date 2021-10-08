07971f175e493567c716e98448a2f659671025bb
Ken <okalang.ds@gmail.com>
2021-10-08 21:22:07 +0200

    Create Model,appsetting adjustment

---
+++ Models/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityApp.Models
+{
+    /// <summary>
+    /// Class provides access to the Product
+    /// objects that EF Core store in the database
+    /// and seeds the database with sample data.
+    /// </summary>
+    /// 
+    public class ProductDbContext
+    {
+    }
+}
